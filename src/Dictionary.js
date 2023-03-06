f25d47c19e476be50ebb9095032306da36dd9a64
isataguchi <122779306+isataguchi@users.noreply.github.com>
2023-03-07 03:41:39 +0900

    added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
+
+  function handlePhotos(response) {
+    setPhotos(response.data.photos);
+  }
+
   function handleResponse(response) {
     setResults(response.data[0]);
+    let photosApiKey = "abao34e9f837fb2339t64098745a7de7";
+    let photosUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photosApiKey}`;
+
+    axios.get(photosUrl).then(handlePhotos);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
   }
 
   function search() {
@@ -24,10 +40,7 @@ export default function Dictionary(props) {
   function handleKeyword(event) {
     setKeyword(event.target.value);
   }
-  function load() {
-    setLoaded(true);
-    search();
-  }
+
   if (loaded) {
     return (
       <div className="Dictionary mt-4 mb-3">
@@ -35,15 +48,15 @@ export default function Dictionary(props) {
           <form onSubmit={handleSubmit}>
             <input
               type="search"
-              value="sunset"
+              defaultValue={props.defaultKeyword}
               autoFocus={true}
               onChange={handleKeyword}
             />
             <input type="submit" value="Search" />
           </form>
         </section>
-
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
