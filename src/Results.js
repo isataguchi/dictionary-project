9b831218a45756f3d38aa0028f2459edca7e204b
isataguchi <122779306+isataguchi@users.noreply.github.com>
2023-03-06 23:55:14 +0900

    added phonetic

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Synonyms from "./Synonyms";
+import Phonetic from "./Phonetic";
+import ReactPlayer from "react-player";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2 className="mt-3">
-          <strong>{props.results.word}</strong> {props.results.phonetic}
+          <strong>{props.results.word}</strong>{" "}
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
         </h2>
 
         {props.results.meanings.map(function (meaning, index) {
